720a7c965677293f510e69ed649e6ab0e016e74a
Branden Colen <brandencolen@gmail.com>
2021-07-07 12:21:06 -0500

    clean up

--- server/app/app.go
+++ server/app/app.go
@@ -7,17 +7,19 @@ import (
 	"github.com/brandenc40/green-mountain-grill/server/logger"
 	"github.com/brandenc40/green-mountain-grill/server/respository"
 	"github.com/brandenc40/green-mountain-grill/server/server"
+	"github.com/sirupsen/logrus"
 	"go.uber.org/fx"
 )
 
 func App() *fx.App {
 	return fx.New(
 		config.Module,
+		grillclient.Module,
+		handler.Module,
 		logger.Module,
 		respository.Module,
-		handler.Module,
 		server.Module,
-		grillclient.Module,
+		fx.Logger(logrus.StandardLogger()),
 		fx.Invoke(server.RegisterRoutes),
 	)
 }
--- server/logger/logger.go
+++ server/logger/logger.go
@@ -10,16 +10,9 @@ import (
 var Module = fx.Provide(New)
 
 func New() *logrus.Logger {
-	level := logrus.DebugLevel
-	if os.Getenv("ENVIRONMENT") == "production" {
-		level = logrus.InfoLevel
+	logger := logrus.StandardLogger()
+	if os.Getenv("ENVIRONMENT") != "production" {
+		logger.SetLevel(logrus.DebugLevel)
 	}
-	logger := &logrus.Logger{
-		Out:       os.Stderr,
-		Formatter: new(logrus.TextFormatter),
-		Hooks:     make(logrus.LevelHooks),
-		Level:     level,
-	}
-	logger.Debug("logging at DEBUG level")
 	return logger
 }
